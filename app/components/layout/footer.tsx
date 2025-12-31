8ddaa4f8c173ac39ff8be672c09a62d8a4bfe34a
vladimir-bogdanovic <vladimirbogdanovic44@gmail.com>
2025-12-31 20:06:12 +0100

    first commit

---
+++ app/components/layout/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="flex items-center justify-center w-full bg-gray-400 mt-10">
+      <p>placeholder</p>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import HeroComponent from "./components/sections/hero";
+
+export default function Home() {
+  return <HeroComponent />;
+}
